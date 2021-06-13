9f9fe454a44eadf63bc9a0fa9979687bea36c450
chdonovan <chrisdonovan311@gmail.com>
2021-06-13 13:50:55 -0500

    category routes assi

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { destroy } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
@@ -61,10 +62,46 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbCategory => {
+      if (!dbCategory) {
+        res.status(404).json({ message: 'No category found'});
+        return;
+      }
+      res.json(dbCategory);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategory => {
+    if (!dbCategory) {
+      res.status(404).json({ message: "no category found"});
+      return;
+    }
+    res.json(dbCategory);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
